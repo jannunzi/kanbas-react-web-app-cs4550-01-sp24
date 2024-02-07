23f8e6e6ad79656e883a006264bfd59604294024
Jose Annunziato <jannunzi@gmail.com>
2024-02-07 11:23:55 -0500

    getting started with JavaScript assignment

---
+++ src/Labs/index.tsx
@@ -0,0 +1,13 @@
+function Labs() {
+  return (
+    <div>
+      <h1>Labs</h1>
+      <h2>Assignment 3</h2>
+      <h3>Exercise 3.1</h3>
+      <h2>Assignment 4</h2>
+      <h2>Assignment 5</h2>
+    </div>
+  );
+}
+
+export default Labs;
